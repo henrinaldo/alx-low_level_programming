1c8e4dd30230a138f2325003f4408e7cce68629a
henrinaldo <adegbolahenry@gmail.com>
2022-09-13 06:33:36 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,22 +4,23 @@
 
 /**
  * main: _putchar
- * Descripion: This program prints _putchar, followed by a new line.
+ * Descripion: This program prints _putchar,
+ * followed by a new line.
  * Return: Always 0 (ssuccess)
  */
 
 int main(void)
 {
 
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
