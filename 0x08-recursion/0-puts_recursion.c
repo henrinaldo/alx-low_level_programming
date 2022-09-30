aa2948cae9e511688d38977ee844ff1fcbb4dce3
henrinaldo <adegbolahenry@gmail.com>
2022-09-30 01:05:47 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,9 +11,9 @@ void _puts_recursion(char *s)
 {
 	if (!*s) /* if s points to null char */
 	{
-		_putchar('\n');
+		putchar('\n');
 		return;
 	}
-	_putchar(*s);
+	putchar(*s);
 	_puts_recursion(s + 1);
 }
